4fa9cceb757733a3df90aefb781e6efee2b31391
bmargerison <bmargerison@outlook.com>
2021-10-25 15:30:55 +0100

    Step 5 - top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -3,5 +3,9 @@ attr_reader :balance
 def initialize
   @balance = 0
 end
+
+def top_up(deposit)
+  @balance += deposit
+end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,5 +5,10 @@ describe Oystercard do
         newcard = Oystercard.new
         expect(newcard.balance).to eq 0
     end
+
+    it 'adds top-up amount to balance' do
+        subject.top_up(50.00)
+        expect(subject.balance).to eq 50.00
+    end
     
 end
