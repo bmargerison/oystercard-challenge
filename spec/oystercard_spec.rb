fac57d86f3f520800928aebd36db407281554bbb
88Andybird <anbird88@gmail.com>
2021-10-25 15:25:29 +0100

    4th challenge added in balance method and attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
+    it 'checks balance is 0 by default' do
+        newcard = Oystercard.new
+        expect(newcard.balance).to eq 0
+    end
     
 end
