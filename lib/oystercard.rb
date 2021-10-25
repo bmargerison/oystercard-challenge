078f8114c40356ac29f9730474a86967a94b137d
88Andybird <anbird88@gmail.com>
2021-10-25 16:03:23 +0100

    Challenge 6 - added account_limit for top up

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,16 @@
 class Oystercard
 attr_reader :balance
+
+ACCOUNT_LIMIT = 90.00
+
 def initialize
   @balance = 0
 end
 
 def top_up(deposit)
-  @balance += deposit
+    raise "balance exceeded #{ACCOUNT_LIMIT}" if (@balance + deposit).to_f > ACCOUNT_LIMIT
+    @balance += deposit
+  
 end
   
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,8 +7,16 @@ describe Oystercard do
     end
 
     it 'adds top-up amount to balance' do
-        subject.top_up(50.00)
-        expect(subject.balance).to eq 50.00
+        newcard = Oystercard.new
+        newcard.top_up(50.00)
+        expect(newcard.balance).to eq 50.00
     end
+
+    it 'throws error if card limit exceeded' do
+        maximum_balance = Oystercard::ACCOUNT_LIMIT
+        subject.top_up(maximum_balance)
+        expect{subject.top_up 1 }.to raise_error(RuntimeError)
+    end
+    
     
 end
